42ccb1d327bd75810da167045d5402ad5bb2df47
QuentinBubu <QuentinBubu.dev@gmail.com>
2021-02-21 21:33:08 +0100

    add animation

--- autres/graven-challenge/js/app.js
+++ autres/graven-challenge/js/app.js
@@ -10,3 +10,33 @@ nav_appli_li.addEventListener('mouseleave', () => {
     nav_appli_span.classList.remove('fa-angle-up');
     nav_appli_span.classList.add('fa-angle-down');
 });
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+window.addEventListener('load', function() {
+    let all = document.querySelectorAll('main > *');
+    all.forEach(element => {
+        setTimeout(function() {
+            element.style.visibility = "visible";
+            element.animate(
+                [
+                    {
+                        transform: 'translateX(-100%)'
+                    },
+                    {
+                        transform: 'translateX(0%)'
+                    }
+                ], {
+                duration: 500,
+                animationFillMode: 'forwards'
+                }
+            );
+        }, 500);
+    });
+});
