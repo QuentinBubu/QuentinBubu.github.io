b45a5dce122cd49a11e1fc803ddb5af6fbcb6999
eKonnect <ekonn63@outlook.fr>
2021-06-23 01:28:51 +0200

    Change Log

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
+
 var header = document.querySelector('header');
 var main = document.querySelector('main');
 var footer = document.querySelector('footer');
@@ -10,14 +16,6 @@ function pageYCheck()
     console.log("[", pageY, "] = pageY");
     wait(2000);
   }
-}
-
-function wait(ms){
-  var start = new Date().getTime();
-  var end = start;
-  while(end < start + ms) {
-    end = new Date().getTime();
- }
 }
 
 let below = false;
@@ -103,3 +101,34 @@ function pbSixOut(){
   document.getElementById('pbd_six').classList.remove('pbd-display');
 }
 // endsection
+
+function turnHeaderFirstImage(){
+  document.getElementById('header_first_image').classList.add('turn-header-first-image');
+}
+
+var open = 0;
+
+function versionInfosDisplay(){
+  if (open == 0){
+    document.getElementById('ver_inf').classList.add('ver-inf-display');
+    document.querySelector('main').classList.add('blur');
+    document.querySelector('header').classList.add('blur');
+    document.querySelector('footer').classList.add('blur');
+    open = 1;
+  }
+  else{
+    document.getElementById('ver_inf').classList.remove('ver-inf-display');
+    document.querySelector('main').classList.remove('blur');
+    document.querySelector('header').classList.remove('blur');
+    document.querySelector('footer').classList.remove('blur');
+    open = 0;
+  }
+}
+
+function verOn(element_id){
+  document.getElementById(element_id).innerHTML = "Changelog";
+}
+
+function verOut(element_id){
+  document.getElementById(element_id).innerHTML = "v.3.0.0";
+}
