0886fcae2cd909de4a29702399ba34029f617304
eKonnect <ekonn63@outlook.fr>
2021-08-09 01:47:43 +0200

    Lol

---
+++ v4/update.js
@@ -0,0 +1,19 @@
+let currentVersion = "4.0.0.0";
+let updateIndicator = document.getElementById('home_header_infos_span');
+
+var requestURL = 'https://version.act-feed.repl.co/qb-io/qb-io.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function(){
+    let jsonObjVersion = request.response;
+    let latestVersion = jsonObjVersion['version'];
+    if (currentVersion === latestVersion){
+        updateIndicator.innerHTML = "Pas de pull request en attente.";
+    }
+    else if (currentVersion !== latestVersion){
+        updateIndicator.innerHTLM = "Pull request en attente. <a>https://github.com/QuentinBubu/QuentinBubu.github.io</a>";
+    }
+}
