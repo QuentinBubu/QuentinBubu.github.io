5be417dee3547f48e61d9bb9670d9ff183989226
eKonnect <ekonn63@outlook.fr>
2021-08-09 13:16:01 +0200

    Menu burger btn

--- v4/script.js
+++ v4/script.js
@@ -4,6 +4,9 @@ let diapo1 = document.getElementById('diapo1');
 let diapo2 = document.getElementById('diapo2');
 let cornerBtn1 = document.getElementById('corner_btn_1');
 let cornerBtn2 = document.getElementById('corner_btn_2');
+let cornerBtn3 = document.getElementById('corner_btn_3');
+let menuBurgerAlpha = document.getElementById('menu_burger_alpha');
+let menuBurger = document.getElementById('menu_burger');
 
 let transitionTime = .5;
 cornerBtn2.onclick = function(){
@@ -14,4 +17,9 @@ cornerBtn2.onclick = function(){
 cornerBtn1.onclick = function(){
     diapo1.style = `transform: scale(1); opacity: 1; transition: ${transitionTime}s;`;
     diapo2.style = `transform: scale(0); opacity: 0; transition: ${transitionTime}s;`;
+}
+
+cornerBtn3.onclick = function(){
+    menuBurgerAlpha.classList.toggle('menu-burger-alpha-toggle');
+    menuBurger.classList.toggle('menu-burger-toggle');
 }
