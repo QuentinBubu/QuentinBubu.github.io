23f43b68d9aa481e34bb9dbe8a484fd024a7dc26
QuentinBubu <quentinbubu.dev@gmail.com>
2025-04-23 11:59:55 +0200

    Fix date

--- assets/js/modalcv.js
+++ assets/js/modalcv.js
@@ -1,3 +1,18 @@
+const steps = document.querySelectorAll('#cv-steps .step');
+    const nextBtn = document.getElementById('next-step');
+    let currentStep = 0;
+
+    nextBtn.addEventListener('click', () => {
+        if (currentStep < steps.length - 1) {
+            steps[currentStep].style.display = 'none';
+            currentStep++;
+            steps[currentStep].style.display = 'block';
+            if (currentStep === steps.length - 1) {
+                nextBtn.style.display = 'none';
+            }
+        }
+    }, false);
+
 function openModal ($el) {
   $el.classList.add('is-active')
 }
